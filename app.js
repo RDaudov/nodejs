b0b18c98388dd68c72d07bc63123ef9803051317
RDaudov <rdaudov007@gmail.com>
2023-09-03 00:46:01 +0300

    start

---
+++ app.js
@@ -0,0 +1,41 @@
+const http  = require('http')
+const fs = require('fs')
+const path = require('path')
+const PORT = 3000;
+const createPath = (page) => path.resolve(`${page}.html`)
+let basePath = ''
+
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+
+    switch(req.url) {
+        case '/':
+            basePath = createPath('index')
+            break;
+        case '/about':
+            basePath = createPath('about')
+            break;
+        default:
+            basePath = createPath('error');
+            break
+    }
+    
+    
+        fs.readFile(basePath, (err, data) => {
+            if (err) {
+                console.log(err);
+                res.end();
+            }
+            else {
+                res.write(data);
+                res.end();
+            }
+        })
+        
+    
+})
+
+server.listen(PORT, 'localhost', (error)  => {
+    error ? console.log(error) : console.log(`listening port ${PORT}`)
+})
---
+++ models/post.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    text: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });
+
+const Post = mongoose.model('Post', postSchema)
+
+module.exports = Post;
